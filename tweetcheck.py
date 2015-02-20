245975be82a7e81ab5f2bda4ad1b6d6f3296925f
Stephen J. Turnbull <stephen@xemacs.org>
2015-02-20 14:35:35 +0900

    Initial draft of tweetcheck.py.

---
+++ tweetcheck.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3.4
+
+"""
+Examine a tweet stream and compute distributions of attributes.
+The tweet stream should be a sequence of tweets in JSON format.
+"""
+
+# #### Implement command-line options!
+
+STREAM = "results/stream.json"          # or "search.json"
+
+with open(STREAM) as stream:            # #### Need to handle Twitter API too.
+    s = stream.read()                   # #### Need online algorithm.
+
+decoder = JSONDecoder()
+tweet, start = decoder.raw_decode(s)
+tweets = [tweet]
+
+while True:
+    try:
+        tweet, start = decoder.raw_decode(s[start:]) # #### Horrible consing?
+    except Exception as e:
+        print("Error:", e)
+        break
+
+print("len(s) =", len(s), "start = ", start, "len(tweets) =", len(tweets))
+print(tweets[0])
