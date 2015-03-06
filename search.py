a5e84c07564eb1571d9b9f75699b19698073dfcb
Stephen J. Turnbull <stephen@xemacs.org>
2015-03-07 04:43:00 +0900

    Loop over movies to query. Add movies, restrict to recent to avoid rate limits.

--- search.py
+++ search.py
@@ -12,36 +12,76 @@ from urllib.error import HTTPError
 import json
 
 # #### Use command line options!!
+MOVIES = [ # "movie",
+#     # Released on Feb. 2
+#     "The DUFF",
+#     "Hot Tub Time Machine 2",
+#     "McFarland, USA",
+#     "Badlapur",
+#     "Digging Up the Marrow",
+#     "Queen and Country",
+#     "Wild Tales",
+#     # Released on Feb. 27
+#     "Focus (2015)",
+#     "The Lazarus Effect",
+#     "'71",
+#     "Deli Man",
+#     "Eastern Boys",
+#     "Everly",
+#     "Farewell to Hollywood",
+#     "Futuro Beach",
+#     "The Hunting Ground",
+#     "A La Mala",
+#     "Maps to the Stars",
+#     "My Life Directed",
+#     "The Salvation",
+#     "Snow Girl and the Dark Crystal",
+#     "Wild Canaries",
+#     # Released on Mar. 6
+    "Chappie",
+    "Unfinished Business",
+    "Bad Asses on the Bayou",
+    "Compared to What? The Improbable Journey of Barney Frank",
+    "Hayride 2",
+    "The Life and Mind of Mark DeFriest",
+    "The Mafia Only Kills in Summer",
+    "Merchants of Doubt",
+    "The Second Best Exotic Marigold Hotel",
+    "These Final Hours",
+    ]
+
 QUERY = "Hobbit movie"
-COUNT = 100
+COUNT = 500
 INDENT = 1
 
 api = get_my_api()
 
-# #### This should be initialized "null".
-results = api.search.tweets(q=QUERY, count=COUNT)
-print(dir(results))
+for QUERY in MOVIES:
+    # #### This should be initialized "null".
+    results = api.search.tweets(q=QUERY, count=COUNT)
+    print(QUERY)
+    print(dir(results))
 
-# #### This too.
-statuses = results['statuses']
-for _ in range(5):
-    print(len(statuses), "received so far.")
-    try:
-        next_query = results['search_metadata']['next_results']
-    except KeyError as e:
-        print("Not a key:", e)
-        break
-    except HTTPError as e:
-        print(e)
-        print("You have probably exceeded a rate limit!!")
-        break
-    except TwitterHTTPError as e:
-        print(e)
-        print("You have probably exceeded a rate limit!!")
-        break
-    kw = dict([kv.split('=') for kv in next_query[1:].split('&')])
-    results = api.search.tweets(**kw)
-    statuses += results['statuses']
+    # #### This too.
+    statuses = results['statuses']
+    for _ in range(5):
+        print(len(statuses), "received so far.")
+        try:
+            next_query = results['search_metadata']['next_results']
+        except KeyError as e:
+            print("Not a key:", e)
+            break
+        except HTTPError as e:
+            print(e)
+            print("You have probably exceeded a rate limit!!")
+            break
+        except TwitterHTTPError as e:
+            print(e)
+            print("You have probably exceeded a rate limit!!")
+            break
+        kw = dict([kv.split('=') for kv in next_query[1:].split('&')])
+        results = api.search.tweets(**kw)
+        statuses += results['statuses']
 print("Done.")
 
 with open("search-results.json", "w") as f:
