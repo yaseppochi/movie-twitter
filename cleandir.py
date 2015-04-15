a442e71dbd2040e1f480faff45c0ace9a4607137
Stephen J. Turnbull <stephen@xemacs.org>
2015-04-15 11:59:33 -0400

    More tests.

--- cleandir.py
+++ cleandir.py
@@ -3,7 +3,7 @@
 import os
 import stat
 
-for i in range(1000000, 1000001):
+for i in range(1000000, 1000003):
     fn = "stream-results-{0:d}.json".format(i)
     s = os.stat(fn)
     sz = s[stat.ST_SIZE]
