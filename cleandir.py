9ec3245512e2084753a75a15a5a1588253e34e72
Stephen J. Turnbull <stephen@xemacs.org>
2015-04-15 11:58:44 -0400

    Fix .format syntax.

--- cleandir.py
+++ cleandir.py
@@ -7,7 +7,7 @@ for i in range(1000000, 1000001):
     fn = "stream-results-{0:d}.json".format(i)
     s = os.stat(fn)
     sz = s[stat.ST_SIZE]
-    print("{0:s} has size {1:d}: ").format(fn, sz, end='')
+    print("{0:s} has size {1:d}: ".format(fn, sz), end='')
     if sz == 0:
         print("removing")
         print("os.unlink(fn)")
