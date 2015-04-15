144a995aa6bc4109994c8b63f5aced21b2a6ab0f
Stephen J. Turnbull <stephen@xemacs.org>
2015-04-15 11:55:33 -0400

    Make cleandir executable.

--- cleandir.py
+++ cleandir.py
@@ -5,7 +5,7 @@ import stat
 
 for i in range(1000000, 1000001):
     fn = "stream-results-{0:d}.json".format(i)
-    s = os.stat(f)
+    s = os.stat(fn)
     sz = s[ST_SIZE]
     print("{0:s} has size {1:d}: ").format(fn, sz, end='')
     if sz == 0:
