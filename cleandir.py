9c73c22ec9755a2f720a17abb0fc539fb1956ca9
Stephen J. Turnbull <stephen@xemacs.org>
2015-04-16 01:32:01 -0400

    Refactor cleanfile() out.  Use enums for status.

--- cleandir.py
+++ cleandir.py
@@ -3,16 +3,42 @@
 import os
 import stat
 
-for i in range(10000, 999999):
-    fn = os.path.join(os.getcwd(), "stream-results-{0:d}.json".format(i))
-    if os.path.exists(fn):
-        s = os.stat(fn)
+ERROR = -1
+NONEXISTENT = 0
+KEPT = 1
+REMOVED = 2
+template = "Files #{0:d} to #{1:d} were {2:s}"
+status_names = ("nonexistent", "kept", "removed")
+
+def cleanfile(path):
+    val = ERROR
+    if os.path.exists(path):
+        s = os.stat(path)
         sz = s[stat.ST_SIZE]
-        print("{0:s} has size {1:d}: ".format(fn, sz), end='')
         if sz == 0:
-            print("removing")
             os.unlink(fn)
+            val = REMOVED
         else:
-            print("keeping")
+            val = KEPT
     else:
-        print("{0:s} does not exist")
+        val = NONEXISTENT
+    return val 
+
+lastchange = None
+old_status = None
+for i in range(10000, 999999):
+    fn = "stream-results-{0:d}.json".format(i)
+    status = cleanfile(os.path.join(os.getcwd(), fn))
+    if old_status is None:
+        lastchange = i
+    elif old_status == status:
+        pass
+    else:
+        if status == ERROR:
+            print("INTERNAL ERROR PROCESSING {0:s}".format(fn))
+            status = old_status          # Hack!
+        else:
+            print(template.format(lastchange, i, status_names[old_status]))
+        lastchange = i
+        old_status = status
+        
