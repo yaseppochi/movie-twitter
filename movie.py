0e21fa0b12831ceec9902193e5dbff684d33f351
Stephen J. Turnbull <stephen@xemacs.org>
2015-05-24 00:29:02 +0900

    Move code from moviedata.py to movie.py.

---
+++ movie.py
@@ -0,0 +1,98 @@
+import moviedata
+
+class MovieException(Exception):
+    pass
+
+class DuplicateNameError(MovieException):
+    pass
+
+class ReinitializationError(MovieException):
+    pass
+
+class Movie(object):
+    """
+    Collect names, opening dates, and stars of movies.
+
+    Fields that are None are uninitialized.  name is required (may not be
+    None), others may be optional or lazily initialized.
+    """
+
+    by_name = {}
+    word_movies = {}
+
+    # #### The optional arguments arguably are useless.
+    def __init__(self, name, opening_date=None, star_list=None):
+        self.name = name
+        self.opening_date = opening_date
+        self.star_list = star_list
+        if name in Movie.by_name:
+             raise DuplicateNameError(name)
+        Movie.by_name[name] = self
+        self._compute_word_movie_maps()
+
+    @classmethod
+    def _get_by_name(cls, name):
+        m = Movie.by_name.get(name)
+        if m is None:
+            m = Movie(name)
+        return m
+
+    def _add_opening_date(self, date):
+        if isinstance(date, str):
+            # #### Need to handle setting time and timezone.
+            raise NotImplementedError(opening_date)
+        if self.opening_date is not None:
+            raise ReinitializationError(self, 'date', date)
+        self.opening_date = date
+
+    def _add_star_list(self, stars):
+        if self.star_list is not None:
+            raise ReinitializationError(self, 'stars', stars)
+        self.star_list = stars
+
+    def __str__(self):
+        if self.star_list:
+            s = ",".join(" " + star for star in self.star_list)
+        elif self.star_list is None:
+            s = " N/A"
+        else:
+            s = " no stars"
+        return "{0} Opened: {1} Stars:{2}".format(self.name,
+                                                  self.opening_date,
+                                                  s)
+
+    # Compute word->movie and movie->word maps.
+    # Note that string "in" operator is case-sensitive, so we lowercase all words.
+    # Movie names are not searched for directly so they don't need lowercasing.
+    def _compute_word_movie_maps(self):
+        self.words = [w.lower() for w
+                      in self.name.translate(moviedata.PUNCT).split()]
+        for w in self.words:
+            # #### Use DefaultDict.
+            wm = Movie.word_movies
+            if w in wm:
+                wm[w].append(self)
+            else:
+                wm[w] = [self]
+
+
+def populate_movie_list(dates, stars):
+    for elt in dates:
+        date = elt[0]
+        for name in elt[1:]:
+            m = Movie._get_by_name(name)
+            m._add_opening_date(date)
+    for elt in stars:
+        m = Movie._get_by_name(elt[0])
+        m._add_star_list(elt[1:])
+
+def track_join(ks):
+    return ','.join(k.translate(moviedata.PUNCT) for k in ks)
+
+populate_movie_list(moviedata.DATES_MOVIES,
+                    moviedata.MOVIES_STARS)
+
+
+if __name__ == "__main__":
+    for m in Movie.by_name.values():
+        print(m)
--- moviedata.py
+++ moviedata.py
@@ -1,5 +1,5 @@
-import itertools
 import datetime
+import itertools
 import pytz
 
 nytime = pytz.timezone('US/Eastern')
@@ -192,9 +192,6 @@ PUNCT = { ord(',') : None,
           ord(';') : None,
           }
 
-def track_join(ks):
-    return ','.join(k.translate(PUNCT) for k in ks)
-
 MOVIES_STARS = [
     ["The DUFF", "Mae Whitman", "Bella Thorne", "Robbie Amell"],
     ["Hot Tub Time Machine 2", "Rob Corddry", "Craig Robinson", "Clark Duke"],
@@ -267,94 +264,3 @@ MOVIES_STARS = [
     ["Far from the Madding Crowd", "Carey Mulligan", "Matthias Schoenaerts", "Michael Sheen"],
     ]
 
-class MovieException(Exception):
-    pass
-
-class DuplicateNameError(MovieException):
-    pass
-
-class ReinitializationError(MovieException):
-    pass
-
-class Movie(object):
-    """
-    Collect names, opening dates, and stars of movies.
-
-    Fields that are None are uninitialized.  name is required (may not be
-    None), others may be optional or lazily initialized.
-    """
-
-    by_name = {}
-    word_movies = {}
-
-    # #### The optional arguments arguably are useless.
-    def __init__(self, name, opening_date=None, star_list=None):
-        self.name = name
-        self.opening_date = opening_date
-        self.star_list = star_list
-        if name in Movie.by_name:
-             raise DuplicateNameError(name)
-        Movie.by_name[name] = self
-        self._compute_word_movie_maps()
-
-    @classmethod
-    def _get_by_name(cls, name):
-        m = Movie.by_name.get(name)
-        if m is None:
-            m = Movie(name)
-        return m
-
-    def _add_opening_date(self, date):
-        if isinstance(date, str):
-            # #### Need to handle setting time and timezone.
-            raise NotImplementedError(opening_date)
-        if self.opening_date is not None:
-            raise ReinitializationError(self, 'date', date)
-        self.opening_date = date
-
-    def _add_star_list(self, stars):
-        if self.star_list is not None:
-            raise ReinitializationError(self, 'stars', stars)
-        self.star_list = stars
-
-    def __str__(self):
-        if self.star_list:
-            s = ",".join(" " + star for star in self.star_list)
-        elif self.star_list is None:
-            s = " N/A"
-        else:
-            s = " no stars"
-        return "{0} Opened: {1} Stars:{2}".format(self.name,
-                                                  self.opening_date,
-                                                  s)
-
-    # Compute word->movie and movie->word maps.
-    # Note that string "in" operator is case-sensitive, so we lowercase all words.
-    # Movie names are not searched for directly so they don't need lowercasing.
-    def _compute_word_movie_maps(self):
-        self.words = [w.lower() for w in self.name.translate(PUNCT).split()]
-        for w in self.words:
-            # #### Use DefaultDict.
-            wm = Movie.word_movies
-            if w in wm:
-                wm[w].append(self)
-            else:
-                wm[w] = [self]
-
-
-def populate_movie_list(dates, stars):
-    for elt in dates:
-        date = elt[0]
-        for name in elt[1:]:
-            m = Movie._get_by_name(name)
-            m._add_opening_date(date)
-    for elt in stars:
-        m = Movie._get_by_name(elt[0])
-        m._add_star_list(elt[1:])
-
-populate_movie_list(DATES_MOVIES, MOVIES_STARS)
-
-
-if __name__ == "__main__":
-    for m in Movie.by_name.values():
-        print(m)
