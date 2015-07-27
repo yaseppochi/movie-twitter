d651eaafddb41f39f4b5c019dbe171fb3b29d592
Stephen J. Turnbull <stephen@xemacs.org>
2015-07-27 11:33:04 +0900

    Interim version of mongoauth with all users.

--- mongoauth.py
+++ mongoauth.py
@@ -1,6 +1,58 @@
-from pymongo import MongoClient as MC
-db=MC(host="127.0.0.1",
-    ssl_certfile="sensei-cert.pem",   # implies ssl=true
-    ssl_ca_certs="demoCA/cacert.pem"
-    ).admin
-db.authenticate(name="emailAddress=turnbull@sk.tsukuba.ac.jp,CN=Sensei,OU=Admin,O=Turnbull Laboratory,ST=Ibaraki,C=JP", mechanism="MONGODB-X509")
+from pymongo import MongoClient
+
+class SSLMongoClient(MongoClient):
+    def __init__(self, distinguished_name, host, user_cert, ca_certs, database):
+        self.distinguished_name = distinguished_name
+        self.host = host
+        self.user_cert = user_cert
+        self.ca_certs = ca_certs
+        self.database = database
+
+    def connect(self):
+        self.client = MongoClient(host=self.host,
+                                  ssl_certfile=self.user_cert,
+                                  ssl_ca_certs=self.ca_certs)
+        db = self.client[self.database]
+        db.authenticate(name=self.distinguished_name, mechanism="MONGODB-X509")
+        return db
+
+local = SSLMongoClient(
+    distinguished_name = "emailAddress=turnbull@sk.tsukuba.ac.jp,"
+                         "CN=Sensei,OU=Admin,"
+                         "O=Turnbull Laboratory,ST=Ibaraki,C=JP",
+    host="127.0.0.1",
+    user_cert="sensei-cert.pem",   # implies ssl=true
+    ca_certs="demoCA/cacert.pem",
+    database="admin"
+    )
+
+sensei = SSLMongoClient(
+    distinguished_name = "emailAddress=turnbull@sk.tsukuba.ac.jp,"
+                         "CN=Sensei,OU=Admin,"
+                         "O=Turnbull Laboratory,ST=Ibaraki,C=JP",
+    host="uwakimon.sk.tsukuba.ac.jp",
+    user_cert="~/sensei-cert.pem",      # implies ssl=true
+    ca_certs="~/cacert.pem",
+    database="admin"
+    )
+
+steve = SSLMongoClient(
+    distinguished_name = "emailAddress=turnbull@sk.tsukuba.ac.jp,"
+                         "CN=Stephen Turnbull,OU=Graduate Seminar,"
+                         "O=Turnbull Laboratory,ST=Ibaraki,C=JP",
+    host="uwakimon.sk.tsukuba.ac.jp",
+    user_cert="~/steve-cert.pem",       # implies ssl=true
+    ca_certs="~/cacert.pem",
+    database="admin"
+    )
+
+anna = SSLMongoClient(
+    distinguished_name = "emailAddress=jdzoomer04@gmail.com,"
+                         "CN=Anna Chen,OU=Graduate Seminar,"
+                         "O=Turnbull Laboratory,ST=Ibaraki,C=JP",
+    host="uwakimon.sk.tsukuba.ac.jp",
+    user_cert="~/anna-cert.pem",      # implies ssl=true
+    ca_certs="~/cacert.pem",
+    database="admin"
+    )
+
