78356e67b10dec1b33016cb5475d0af2a8efc8be
Stephen J. Turnbull <stephen@xemacs.org>
2015-11-30 23:42:57 +0900

    Add test.

---
+++ tests/test_parse_command_line_4.py
@@ -0,0 +1,16 @@
+#! /opt/local/bin/python3.4
+
+# test setup
+import sys
+sys.path.append("..")
+
+# test data
+sys.argv[1:] = ["data"]
+
+# test scaffolding
+from partition_tweets import parse_command_line
+
+json_files = parse_command_line()
+for file in json_files:
+    print(file)
+
