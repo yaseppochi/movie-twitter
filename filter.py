b1e92deeea5eb00b0e529847a1f824cc425bcc2b
Stephen J. Turnbull <stephen@xemacs.org>
2015-12-08 16:21:14 +0900

    Fix thinko.

--- filter.py
+++ filter.py
@@ -345,7 +345,7 @@ if __name__ == "__main__":
                 "Average urls per tweet",
                 "Hash repeat count",
                 "Week",
-                ],
+                ]),
               file=f)
     
     # get movie list
